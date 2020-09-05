622010fa576d026621f737feb40375deb53489e6
zik <ziakhorsandi2@gmail.com>
2020-09-05 19:23:54 +0430

    Updates

---
+++ precache-manifest.93833250bd0a68cc85c510e15747af20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdcd3436246c39ed901768687e9c5a93",
+    "url": "/Pagination-with-useContext-bootstrap/index.html"
+  },
+  {
+    "revision": "5dede45afb90d37824b0",
+    "url": "/Pagination-with-useContext-bootstrap/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "5dede45afb90d37824b0",
+    "url": "/Pagination-with-useContext-bootstrap/static/js/2.ef08db97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pagination-with-useContext-bootstrap/static/js/2.ef08db97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61589e9645d7d68f4aa4",
+    "url": "/Pagination-with-useContext-bootstrap/static/js/main.341a02a2.chunk.js"
+  },
+  {
+    "revision": "705c212a6438456d44ec",
+    "url": "/Pagination-with-useContext-bootstrap/static/js/runtime-main.66f3b66a.js"
+  }
+]);
